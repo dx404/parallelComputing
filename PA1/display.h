1886d4ffe6c6458daf2bbcf3d4cc9cd966791136
Duo Zhao <duozhao@cs.unc.edu>
2013-09-20 20:33:54 -0400

    current

--- PA1/display.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void bodiesPrint(Body bodies[], int start, int end);
-
-#endif
--- PA1/timeTest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-
-int main(){
-	time_t x = time(NULL);
-	printf("%d\n", x);
-	return 0;
-}
